ad31cab5d3202e7bcc8162bc94505f2ac8e6d621
mrdooshim <mr.dooshim@gmail.com>
2025-09-07 17:22:52 -0400

    Update stg_users.sql

--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -2,7 +2,7 @@ WITH base AS (
   SELECT
     CAST(id AS VARHCAR) AS user_id,
     CAST(created AS DATE) AS created_date,
-    LOWER(trim(title)) AS title,
+    LOWER(TRIM(title)) AS title,
   FROM {{ source('raw','users') }}
   )
 SELECT * FROM base;
