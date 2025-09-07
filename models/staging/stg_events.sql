82f1db70972a46dbd8f11f907f033bfc11a8db4d
mrdooshim <mr.dooshim@gmail.com>
2025-09-07 17:23:50 -0400

    Update stg_events.sql

--- models/staging/stg_events.sql
+++ models/staging/stg_events.sql
@@ -1,11 +1,11 @@
 WITH base AS (
   SELECT
-  CAST(created AS timestamp) AS created,
-  CAST(firm_id AS varchar) AS firm_id,
-  CAST(user_id AS varchar) AS user_id,
-  lower(trim(event_type)) AS event_type,
-  CAST(num_docs AS int) AS num_docs,
-  CAST(feedback_score AS int) AS feedback_score,
+  CAST(created AS TIMESTAMP) AS created,
+  CAST(firm_id AS VARCHAR) AS firm_id,
+  CAST(user_id AS VARCHAR) AS user_id,
+  LOWER(TRIM(event_type)) AS event_type,
+  CAST(num_docs AS INT) AS num_docs,
+  CAST(feedback_score AS INT) AS feedback_score
 FROM {{ source('raw','events') }}
   )
 SELECT * FROM base;
