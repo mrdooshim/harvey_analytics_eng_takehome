0c6f49a80432dd81fac48c89eec2a2be54cc8c2a
mrdooshim <mr.dooshim@gmail.com>
2025-09-07 17:24:11 -0400

    Update stg_firms.sql

--- models/staging/stg_firms.sql
+++ models/staging/stg_firms.sql
@@ -1,7 +1,7 @@
 WITH base AS (
   SELECT 
     CAST(id AS VARCHAR) AS firm_id,
-    CAST(created AS DATE) AS created_date
+    CAST(created AS DATE) AS created_date,
     CAST(firm_size AS INT) AS firm_size,
     CAST(arr_in_thousands AS INT) AS arr_in_thousands_usd
 FROM {{ source('raw','firms') }}
