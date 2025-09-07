739a50845ae6d9ec70b3275cb436e6edbc465d09
mrdooshim <mr.dooshim@gmail.com>
2025-09-07 17:21:13 -0400

    Create stg_firms.sql

---
+++ models/staging/stg_firms.sql
@@ -0,0 +1,9 @@
+WITH base AS (
+  SELECT 
+    CAST(id AS VARCHAR) AS firm_id,
+    CAST(created AS DATE) AS created_date
+    CAST(firm_size AS INT) AS firm_size,
+    CAST(arr_in_thousands AS INT) AS arr_in_thousands_usd
+FROM {{ source('raw','firms') }}
+  )
+SELECT * FROM base;
