11375c04f366d24f9a254da5007684da21ebd006
mrdooshim <mr.dooshim@gmail.com>
2025-09-07 17:22:23 -0400

    Create stg_users.sql

---
+++ models/staging/stg_users.sql
@@ -0,0 +1,8 @@
+WITH base AS (
+  SELECT
+    CAST(id AS VARHCAR) AS user_id,
+    CAST(created AS DATE) AS created_date,
+    LOWER(trim(title)) AS title,
+  FROM {{ source('raw','users') }}
+  )
+SELECT * FROM base;
