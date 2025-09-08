518af011289046c122cd360088ce99d381eaa97e
mrdooshim <mr.dooshim@gmail.com>
2025-09-07 22:57:19 -0400

    Update stg_events.sql

--- models/staging/stg_events.sql
+++ models/staging/stg_events.sql
@@ -3,7 +3,7 @@ WITH base AS (
   CAST(created AS TIMESTAMP) AS created,
   CAST(firm_id AS VARCHAR) AS firm_id,
   CAST(user_id AS VARCHAR) AS user_id,
-  LOWER(TRIM(event_type)) AS event_type,
+  UPPER(event_type) AS event_type,
   CAST(num_docs AS INT) AS num_docs,
   CAST(feedback_score AS INT) AS feedback_score
 FROM {{ source('raw','events') }}
